d07fb273cc72f8ce462e5e9075d0c92049135df2
vagrant <vagrant@homestead>
2022-11-12 12:51:44 +0000

    Lesson 4 completed - Model test

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title></title>
 </head>
+
 <body>
-<h1>Birdboard</h1>  
-<ul>
-  @foreach($projects as $project) 
-  <li>{{ $project->title }}</li>
-  @endforeach
-</ul>
+  <h1>Birdboard</h1>
+  <ul>
+    @forelse($projects as $project)
+      <li>
+        <a href="/projects/{{ $project->id }}">{{ $project->title }}</a>
+      </li>
+    @empty
+      <li>No projects yet</li>
+    @endforelse
+  </ul>
 </body>
+
 </html>
